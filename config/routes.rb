1002de1cb325b7a23a0ece6020bd69702c57049a
Rawan <rawanalex15@gmail.com>
2022-07-03 17:32:21 +0200

    Athentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Temporary
+  resources :users
+
+  # TODO: root will be homepage containing project link
+  # Leave this temporary
+  root "users#index"
 end
