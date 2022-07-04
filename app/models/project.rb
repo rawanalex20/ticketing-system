38a4c302458d7e545f4c073bd323e587091b97b9
Rawan <rawanalex15@gmail.com>
2022-07-04 08:09:58 +0200

    association added between user and projects as creator and editors

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
-    belongs_to :user, class_name: 'Creator'
+    belongs_to :creator, class_name: 'User'
+    has_and_belongs_to_many :editors, class_name: 'User'
+    
 end
