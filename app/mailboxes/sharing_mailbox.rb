99d24b3507552e4c6237c071250873388c9405e9
Rawan <rawanalex15@gmail.com>
2022-07-05 20:54:57 +0200

    Action mailbox configured

---
+++ app/mailboxes/sharing_mailbox.rb
@@ -0,0 +1,30 @@
+class SharingMailbox < ApplicationMailbox
+  before_processing :find_user
+  MATCHER = /reply-(.+)@reply.example.com/i 
+  # mail => Mail object
+   # inbound_emails => ActionMailbox::InboundEmail record
+  
+   # before_processing :ensure_user
+
+   def process
+    return if user.nil?
+  end
+
+  def user
+    @user ||= User.find_by(email: mail.from)
+  end
+
+  def ensure_user
+    if user.nil?
+      bounce_with UserMailer.missing(inbound_email)
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(email: mail.from)
+  end
+  # recipient = mail.recipients.find{ |r| MATCHER.match?(r)}
+  # recipient[MATCHER, 1] to run matcher against and the first capture group
+end
