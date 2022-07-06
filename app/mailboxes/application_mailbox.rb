5fef66b7d9b344ccd576a98f883a0764fbf95ff7
Rawan <rawanalex15@gmail.com>
2022-07-06 02:03:20 +0200

    method removed

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,5 +1,4 @@
 class ApplicationMailbox < ActionMailbox::Base
-  skip_before_action :verify_authenticity_token
   # http://localhost:3000/rails/conductor/action_mailbox/inboud_emails/new
   # ngrok
   # routing /something/i => :somewhere
