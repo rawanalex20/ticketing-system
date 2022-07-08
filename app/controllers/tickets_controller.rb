b6a131583adad3dd2e65b81bb32e8460e9c84b84
Rawan <rawanalex15@gmail.com>
2022-07-08 09:42:53 +0200

    button edited

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -102,7 +102,7 @@ class TicketsController < ApplicationController
     @ticket.destroy
 
     respond_to do |format|
-      format.html { redirect_to project_tickets_url, notice: "Ticket was successfully destroyed." }
+      format.html { redirect_to project_tickets_url, notice: "Ticket was successfully deleted." }
       format.json { head :no_content }
     end
   end
