f259a167d2f365e40760f54a2e24f44dcbe8dd68
Rawan <rawanalex15@gmail.com>
2022-07-08 20:51:24 +0200

    ..

--- app/jobs/send_invite_job.rb
+++
@@ -1,7 +0,0 @@
-class SendInviteJob < ApplicationJob
-  queue_as :default
-
-  def perform(project, email, user)
-    InviteMailer.with(project: project, email: email, user: user).new_invite_email.deliver_now
-  end
-end
